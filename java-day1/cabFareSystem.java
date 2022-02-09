2e44727fa3f83d7adf48a553205e615a3fc6a7ad
GANESAN R <88224886+GANESANGIDON@users.noreply.github.com>
2022-02-09 17:38:07 +0530

    Update cabFareSystem.java

--- java-day1/cabFareSystem.java
+++ java-day1/cabFareSystem.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
 
+class cab {
+  int fare;
+  
+  cab(){
+    fare =30; // initial registeration fee
+  }
+
+  cab(int fee){
+    fare = fee; // registeration fee if the distance of nearby cab is more (>5)
+  }
+
+  void cabFare(){
+    System.out.println("The total fare amount is " + fare ); // Total fare
+    System.out.println("Thankyou for using our service, enjoy the ride!"); // greetings
+  }
+}
+
+class cabRide{
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.println("Enter the distance of the  nearby available cab (in km) : ");
+      int cd = sc.nextInt(); // nearby cab distance
+      System.out.println("Enter the travel distance (in km) : ");
+      int td = sc.nextInt(); //travel distance
+      if(cd <= 5){
+        cab user = new cab(); // calling default constructor
+        user.fare += 10 * td;
+        user.cabFare();
+      }
+      else{
+        cab user = new cab(30 + (cd - 5) * 10); // calling parameterized constructor
+        user.fare += 10 * td;
+        user.cabFare();
+      }
+    }
+  }
+} 
